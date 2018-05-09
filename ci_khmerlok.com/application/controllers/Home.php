67ee83696607885da7cc50dd17223364bfe3d264
Rothnak <32561213+Rothnak@users.noreply.github.com>
2018-05-09 11:28:57 +0700

    update

--- ci_khmerlok.com/application/controllers/Home.php
+++ ci_khmerlok.com/application/controllers/Home.php
@@ -1,9 +1,18 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Home extends CI_Controller {
+    
+  function __construct() {
+      parent::__construct();      
+      $this->load->helper('text');
+      //$this->load->database();
+      //$this->perPage = 5;
+     // $this->starOrder=4;
 
-	
+      // $this->load->model('Crud_model','m_crud',True);    
+
+      date_default_timezone_set('Asia/Phnom_Penh');
+
+	}
 	public function index()
 	{
 		$this->load->view('home');
@@ -11,3 +20,4 @@ class Home extends CI_Controller {
 	}
 
 }
+
