90a83ced18f0fc35f89554528fc4baf42880d962
Rothnak <32561213+Rothnak@users.noreply.github.com>
2018-05-15 21:49:50 +0700

    front page
    by Rothnak

---
+++ ci_khmerlokk.com/application/views/includes/v_products.php
@@ -0,0 +1,136 @@
+<div class="search-result-container ">
+          <div id="myTabContent" class="tab-content category-list">
+            <div class="tab-pane active " id="grid-container">
+              <div class="category-product">
+                <div class="row">
+
+                  <?php  for ($i=1; $i <=11 ; $i++) { ?>
+                     <div class="col-sm-6 col-md-4 wow fadeInUp">
+                        <div class="products">
+                          <div class="product">
+                            <div class="product-image">
+                              <div class="image"> <a href=""><img  src="assets/images/products/p<?php echo $i ?>.jpg" alt=""></a> </div>
+                              <!-- /.image -->
+                              
+                              <div class="tag new"><span>new</span></div>
+                            </div>
+                            <!-- /.product-image -->
+                            
+                            <div class="product-info text-left">
+                              <h3 class="name"><a href="">Floral Print Buttoned</a></h3>
+                              <div class="rating rateit-small"></div>
+                              <div class="description"></div>
+                              <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$800</span> </div>
+                              <!-- /.product-price --> 
+                              
+                            </div>
+                            <!-- /.product-info -->
+                            <div class="cart clearfix animate-effect">
+                              <div class="action">
+                                <ul class="list-unstyled">
+                                  <li class="add-cart-button btn-group">
+                                    <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
+                                    <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                                  </li>
+                                  <li class="lnk wishlist"> <a class="add-to-cart" href="detail" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+                                  <li class="lnk"> <a class="add-to-cart" href="detail" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
+                                </ul>
+                              </div>
+                              <!-- /.action --> 
+                            </div>
+                            <!-- /.cart --> 
+                          </div>
+                          <!-- /.product --> 
+                          
+                        </div>
+                        <!-- /.products --> 
+                      </div>
+                      <!-- /.item -->
+                    <?php } ?>
+                  
+                </div>
+                <!-- /.row --> 
+              </div>
+              <!-- /.category-product --> 
+              
+            </div>
+            <!-- /.tab-pane -->
+            
+            <div class="tab-pane "  id="list-container">
+              <div class="category-product">
+                <?php  for ($i=1; $i <=11 ; $i++) { ?>
+                <div class="category-product-inner wow fadeInUp">
+                  <div class="products">
+                    <div class="product-list product">
+                      <div class="row product-list-row">
+                        <div class="col col-sm-4 col-lg-4">
+                          <div class="product-image">
+                            <div class="image"> <img src="assets/images/products/p<?php echo $i ?>.jpg" alt=""> </div>
+                          </div>
+                          <!-- /.product-image --> 
+                        </div>
+                        <!-- /.col -->
+                        <div class="col col-sm-8 col-lg-8">
+                          <div class="product-info">
+                            <h3 class="name"><a href="">Floral Print Buttoned</a></h3>
+                            <div class="rating rateit-small"></div>
+                            <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$800</span> </div>
+                            <!-- /.product-price -->
+                            <div class="description m-t-10">Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget, lacinia id purus. Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget.</div>
+                            <div class="cart clearfix animate-effect">
+                              <div class="action">
+                                <ul class="list-unstyled">
+                                  <li class="add-cart-button btn-group">
+                                    <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
+                                    <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                                  </li>
+                                  <li class="lnk wishlist"> <a class="add-to-cart" href="detail" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+                                  <li class="lnk"> <a class="add-to-cart" href="detail" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
+                                </ul>
+                              </div>
+                              <!-- /.action --> 
+                            </div>
+                            <!-- /.cart --> 
+                            
+                          </div>
+                          <!-- /.product-info --> 
+                        </div>
+                        <!-- /.col --> 
+                      </div>
+                      <!-- /.product-list-row -->
+                      <div class="tag new"><span>new</span></div>
+                    </div>
+                    <!-- /.product-list --> 
+                  </div>
+                  <!-- /.products --> 
+                </div>
+                <!-- /.category-product-inner -->
+                <?php } ?>
+               
+                
+              </div>
+              <!-- /.category-product --> 
+            </div>
+            <!-- /.tab-pane #list-container --> 
+          </div>
+          <!-- /.tab-content -->
+          <div class="clearfix filters-container">
+            <div class="text-right">
+              <div class="pagination-container">
+                <ul class="list-inline list-unstyled">
+                  <li class="prev"><a href="#"><i class="fa fa-angle-left"></i></a></li>
+                  <li><a href="#">1</a></li>
+                  <li class="active"><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li class="next"><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                </ul>
+                <!-- /.list-inline --> 
+              </div>
+              <!-- /.pagination-container --> </div>
+            <!-- /.text-right --> 
+            
+          </div>
+          <!-- /.filters-container --> 
+          
+        </div>
