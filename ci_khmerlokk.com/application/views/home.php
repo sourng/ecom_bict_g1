9efc8c789ee83dd5c8fc9eeff05c82b1b4f4f0e4
Rothnak <32561213+Rothnak@users.noreply.github.com>
2018-05-23 08:39:40 +0700

    update

--- ci_khmerlokk.com/application/views/home.php
+++ ci_khmerlokk.com/application/views/home.php
@@ -1,36 +1,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<!-- Meta -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<meta name="description" content="">
-<meta name="author" content="">
-<meta name="keywords" content="MediaCenter, Template, eCommerce">
-<meta name="robots" content="all">
-<title>Shopping</title>
-
-<!-- Bootstrap Core CSS -->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
-
-<!-- Customizable CSS -->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/blue.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.carousel.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.transitions.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.min.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/rateit.css">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-select.min.css">
-
-<!-- Icons/Glyphs -->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.css">
-
-<!-- Fonts -->
-<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-</head>
+  <?php
+    $this->load->view('includes/v_header');
+  ?>
 <body class="cnt-home">
 <!-- ============================================== HEADER ============================================== -->
 <header class="header-style-1"> 
@@ -210,23 +183,11 @@
 
 $this->load->view('includes/v_brands_carousel');
 $this->load->view('includes/v_footer');
+
 ?>
 
 
 
 
-<!-- JavaScripts placed at the end of the document so the pages load faster --> 
-<script src="<?php echo base_url();?>assets/js/jquery-1.11.1.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/bootstrap-hover-dropdown.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/echo.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/jquery.easing-1.3.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/bootstrap-slider.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/jquery.rateit.min.js"></script> 
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/lightbox.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/bootstrap-select.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/wow.min.js"></script> 
-<script src="<?php echo base_url();?>assets/js/scripts.js"></script>
 </body>
 </html>
--- ci_khmerlokk.com/application/views/includes/v_footer.php
+++ ci_khmerlokk.com/application/views/includes/v_footer.php
@@ -126,15 +126,17 @@
 <!-- For demo purposes – can be removed on production : End --> 
 
 <!-- JavaScripts placed at the end of the document so the pages load faster --> 
-<script src="assets/js/jquery-1.11.1.min.js"></script> 
-<script src="assets/js/bootstrap.min.js"></script> 
-<script src="assets/js/bootstrap-hover-dropdown.min.js"></script> 
-<script src="assets/js/owl.carousel.min.js"></script> 
-<script src="assets/js/echo.min.js"></script> 
-<script src="assets/js/jquery.easing-1.3.min.js"></script> 
-<script src="assets/js/bootstrap-slider.min.js"></script> 
-<script src="assets/js/jquery.rateit.min.js"></script> 
-<script type="text/javascript" src="assets/js/lightbox.min.js"></script> 
-<script src="assets/js/bootstrap-select.min.js"></script> 
-<script src="assets/js/wow.min.js"></script> 
-<script src="assets/js/scripts.js"></script>
+
+<!-- JavaScripts placed at the end of the document so the pages load faster --> 
+<script src="<?php echo base_url();?>assets/js/jquery-1.11.1.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/bootstrap-hover-dropdown.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/echo.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/jquery.easing-1.3.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/bootstrap-slider.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/jquery.rateit.min.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/lightbox.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/bootstrap-select.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/wow.min.js"></script> 
+<script src="<?php echo base_url();?>assets/js/scripts.js"></script>
